a4dfc9edd38d477a6c029853a94dc77e1bbc586b
James Mwendwa <mwendwajames32@gmail.com>
2022-09-09 18:40:48 +0300

    Arrays & Tuples

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Basic Types
+let id = 5;
+let company = 'Kings Worth';
+let isPublished = true;
+let x = 'Hello tsc';
+let ids = [1, 2, 3];
+let arr = [1, 2, 'three'];
+// Tuple
+let person = [1, 'James', 'james', true];
+// Tuple array
+let employees;
+employees = [
+    [1, 'james'],
+    [2, 'jane'],
+    [3, 'john']
+];
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Basic Types
+
+let id: number = 5;
+let company: string = 'Kings Worth'
+let isPublished: boolean = true
+let x: any = 'Hello tsc'
+
+let ids: number[] = [1,2,3]
+let arr: any[] = [1,2,'three']
+
+
+// Tuple
+
+let person: [number, string, string, boolean] = [1, 'James', 'james', true]
+
+// Tuple array
+
+let employees: [number, string][]
+
+employees = [
+    [1, 'james'],
+    [2, 'jane'],
+    [3, 'john']
+]
